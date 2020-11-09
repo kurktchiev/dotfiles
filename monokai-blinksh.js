43b95fb0a079efbd0e90ec13bb659be6845ea62f
Boris Kurktchiev <kurktchiev@gmail.com>
2020-11-09 14:30:32 +0100

    Create monokai-blinksh.js

---
+++ monokai-blinksh.js
@@ -0,0 +1,10 @@
+t.prefs_.set('color-palette-overrides',["#1a1a1a", "#f92672", "#a6e22e", "#fd971f", "#ae81ff", "#f92672", "#66d9ef", "#c4c5b5", "#625e4c", "#f92672", "#a6e22e", "#fd971f", "#ae81ff", "#f92672", "#66d9ef", "#f6f6ef"]);
+t.prefs_.set('foreground-color', "#c4c5b5");
+t.prefs_.set('background-color', "#1a1a1a");
+t.prefs_.set('cursor-color', 'rgba(246,247,236,0.5)');
+
+// f92672 - magenta
+// a6e22e - green
+// 66d9ef - blue
+// fd971f - orange
+// ae81ff - purple
